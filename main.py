20dd94ffcad0b1f8f759de74e82b067124a99b88
cvazcosta <cvazcosta2001@gmail.com>
2025-12-26 12:05:01 -0300

    add sysargv for catching the path file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, chars_dict_to_sorted_list
 
 def get_book_text(path_to_file):
@@ -6,10 +7,14 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {sys.argv[1]}...")
   print("----------- Word Count ----------")
-  text = get_book_text("books/frankenstein.txt")
+  text = get_book_text(sys.argv[1])
   print(f"Found {count_words(text)} total words")
   
   print("--------- Character Count -------")
